26cbd110c52bb13eec547e6ed1ef9c1f77d84d6a
shivanigautam <40173904+shivanigautam@users.noreply.github.com>
2021-01-30 21:55:43 +0530

    Add files via upload

--- PaymentMethod.py
+++ PaymentMethod.py
@@ -2,10 +2,10 @@
 class PaymentMethod:
 
     def PremiumPaymentGateWay(self):
-        return "This is premium payment method"
+        return "This is premium payment method!!"
 
     def ExpensivePaymentGateWay(self):
-        return "This is expensive"
+        return "This is expensive!!"
 
     def CheapPaymentGateWay(self):
-        return "This is cheap payment method"
+        return "This is cheap payment method!!"
